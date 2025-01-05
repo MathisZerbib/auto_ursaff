a9178345081e376dc4125b2bc931d4f1902b1f6b
MathisZerbib <mathis.zerbib@gmail.com>
2025-01-05 23:06:01 +0100

    try to fix deploy lint webhook removed hook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,73 +1,73 @@
-import type { Stripe } from "stripe";
-import { NextResponse } from "next/server";
-import getStripe from "@/utils/get-stripe";
+// import type { Stripe } from "stripe";
+// import { NextResponse } from "next/server";
+// import getStripe from "@/utils/get-stripe";
 
-export async function POST(req: Request) {
-  const stripe = await getStripe();
+// export async function POST(req: Request) {
+//   const stripe = await getStripe();
 
-  if (!stripe) {
-    const errorMessage = "Stripe initialization failed";
-    console.log(`❌ Error message: ${errorMessage}`);
-    return NextResponse.json(
-      { message: `Webhook Error: ${errorMessage}` },
-      { status: 500 }
-    );
-  }
+//   if (!stripe) {
+//     const errorMessage = "Stripe initialization failed";
+//     console.log(`❌ Error message: ${errorMessage}`);
+//     return NextResponse.json(
+//       { message: `Webhook Error: ${errorMessage}` },
+//       { status: 500 }
+//     );
+//   }
 
-  let event: Stripe.Event;
+//   let event: Stripe.Event;
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      await (await req.blob()).text(),
-      req.headers.get("stripe-signature") as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string
-    );
-  } catch (err) {
-    const errorMessage = err instanceof Error ? err.message : "Unknown error";
-    console.log(`❌ Error message: ${errorMessage}`);
-    return NextResponse.json(
-      { message: `Webhook Error: ${errorMessage}` },
-      { status: 400 }
-    );
-  }
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       await (await req.blob()).text(),
+//       req.headers.get("stripe-signature") as string,
+//       process.env.STRIPE_WEBHOOK_SECRET as string
+//     );
+//   } catch (err) {
+//     const errorMessage = err instanceof Error ? err.message : "Unknown error";
+//     console.log(`❌ Error message: ${errorMessage}`);
+//     return NextResponse.json(
+//       { message: `Webhook Error: ${errorMessage}` },
+//       { status: 400 }
+//     );
+//   }
 
-  console.log("✅ Success:", event.id);
+//   console.log("✅ Success:", event.id);
 
-  const permittedEvents: string[] = [
-    "checkout.session.completed",
-    "payment_intent.succeeded",
-    "payment_intent.payment_failed",
-  ];
+//   const permittedEvents: string[] = [
+//     "checkout.session.completed",
+//     "payment_intent.succeeded",
+//     "payment_intent.payment_failed",
+//   ];
 
-  if (permittedEvents.includes(event.type)) {
-    let data;
+//   if (permittedEvents.includes(event.type)) {
+//     let data;
 
-    try {
-      switch (event.type) {
-        case "checkout.session.completed":
-          data = event.data.object as Stripe.Checkout.Session;
-          console.log(`💰 CheckoutSession status: ${data.payment_status}`);
-          break;
-        case "payment_intent.payment_failed":
-          data = event.data.object as Stripe.PaymentIntent;
-          console.log(`❌ Payment failed: ${data.last_payment_error?.message}`);
-          break;
-        case "payment_intent.succeeded":
-          data = event.data.object as Stripe.PaymentIntent;
-          console.log(`💰 PaymentIntent status: ${data.status}`);
-          break;
-        default:
-          throw new Error(`Unhandled event: ${event.type}`);
-      }
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "Unknown error";
-      console.log(`❌ Error handling event: ${errorMessage}`);
-      return NextResponse.json(
-        { message: `Event Handling Error: ${errorMessage}` },
-        { status: 500 }
-      );
-    }
-  }
+//     try {
+//       switch (event.type) {
+//         case "checkout.session.completed":
+//           data = event.data.object as Stripe.Checkout.Session;
+//           console.log(`💰 CheckoutSession status: ${data.payment_status}`);
+//           break;
+//         case "payment_intent.payment_failed":
+//           data = event.data.object as Stripe.PaymentIntent;
+//           console.log(`❌ Payment failed: ${data.last_payment_error?.message}`);
+//           break;
+//         case "payment_intent.succeeded":
+//           data = event.data.object as Stripe.PaymentIntent;
+//           console.log(`💰 PaymentIntent status: ${data.status}`);
+//           break;
+//         default:
+//           throw new Error(`Unhandled event: ${event.type}`);
+//       }
+//     } catch (error) {
+//       const errorMessage = error instanceof Error ? error.message : "Unknown error";
+//       console.log(`❌ Error handling event: ${errorMessage}`);
+//       return NextResponse.json(
+//         { message: `Event Handling Error: ${errorMessage}` },
+//         { status: 500 }
+//       );
+//     }
+//   }
 
-  return NextResponse.json({ message: "Received" }, { status: 200 });
-}
+//   return NextResponse.json({ message: "Received" }, { status: 200 });
+// }
